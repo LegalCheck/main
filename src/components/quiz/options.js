bc5f6ca9db00c98165417226899af392f946f6e6
gabrielamelian <gabrielamelian@gmail.com>
2017-03-18 16:44:13 +1300

    Fix issues on initstate, adds options component

--- src/components/quiz/options.js
+++ src/components/quiz/options.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Options = () => (
   <div>
-    <h3>Options! This is a placeholder</h3>
+    <div>
+      <input type="button" name="answer" value="Y"/>
+      <input type="button" name="answer" value="N"/>
+      <input type="button" name="answer" value="Unsure"/>
+    </div>
+    <div>
+      <input type="button" name="navigate" value="Back"/>
+      <input type="button" name="navigate" value="Next"/>
+    </div>
   </div>
 )
 
