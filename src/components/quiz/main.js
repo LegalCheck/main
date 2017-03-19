e361c9907f19d78c6a72a6d84967fabe523944f3
katenorquay <katenorquay@live.com>
2017-03-19 14:04:53 +1300

    commit to merge

--- src/components/quiz/main.js
+++ src/components/quiz/main.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Options from './options'
 import QuestionText from './question'
 import ProgressBar from '../progressBar'
+import Progress from '../../utilities/progress'
 
-const QuizPage = ({state, dispatch}) => (
-  <div>
-    <ProgressBar state={state} dispatch={dispatch}/>
-    <QuestionText state={state} dispatch={dispatch}/>
-    <Options state={state} dispatch={dispatch}/>
-  </div>
-)
+const QuizPage = ({state, dispatch}) => {
+  return (
+    <div>
+      <ProgressBar state={state} dispatch={dispatch}/>
+      <QuestionText state={state} dispatch={dispatch}/>
+      <Options state={state} dispatch={dispatch}/>
+    </div>
+  )
+}
 
 export default QuizPage
