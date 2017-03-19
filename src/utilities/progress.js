cdad0fa85556529e9c0cb0e78dfe4e983201720c
kfrn <knfrances@gmail.com>
2017-03-19 13:59:42 +1300

    Add preface to relationship Qs

--- src/utilities/progress.js
+++ src/utilities/progress.js
@@ -1,7 +1,6 @@
 function Progress(progress) {
   var speechContainer = document.getElementsByClassName("js-speechContainer")[0];
   var speechDirection = document.getElementsByClassName("js-direction")[0];
-  console.log('this is speech', speechContainer)
 
   speechContainer.style.left = "calc("+progress+"% - "+ speechContainer.offsetWidth*progress/100+"px)";
   speechDirection.style.left = "calc("+progress+"% - "+ speechDirection.offsetWidth*progress/100+"px - "+10*progress/100+"px +  "+10*(1/(progress+1))+"px)";
