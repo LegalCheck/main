d1368ca0b3388b10cde86e4cf5131297d3130b01
kfrn <knfrances@gmail.com>
2017-03-19 18:45:36 +1300

    Wrap answer in object; log that object

--- src/utilities/createPDF.js
+++ src/utilities/createPDF.js
@@ -1,5 +1,11 @@
 function createPDF(state) {
-  console.log(state)
+  let answers = state.map(elem => {
+    return {
+      "issue": elem.statement,
+      "answer": elem.answer
+    }
+  })
+  console.log(answers)
 }
 
 module.exports = createPDF
